a3f36b04161438f00e166121992569b99c5c4ba7
swarnendu19 <swarnendumaity633@gmail.com>
2023-12-09 11:36:14 +0530

    Assignment

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,15 @@
 */
 
 function isAnagram(str1, str2) {
+  const sortedStr1 = str1.split('').sort().join('');
+  const sortedStr2 = str2.split('').sort().join('');
 
+    if(sortedStr1===sortedStr2 ){
+       return true
+    }
+    else{
+      return false
+     }
 }
 
 module.exports = isAnagram;
--- 01-js/easy/expenditure-analysis.js
+++ 01-js/easy/expenditure-analysis.js
@@ -7,9 +7,54 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-expenditure-analysis`
 */
-
+const transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,        
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: 'Food',
+    itemName: 'Burger',
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: 'Clothing',
+    itemName: 'T-Shirt',
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: 'Electronics',
+    itemName: 'Headphones',
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: 'Clothing',
+    itemName: 'Jeans',
+  },
+];
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+
+
+transactions.forEach((transaction)=>{
+
+result+= transaction.price
+
+})
+return result
 }
 
+
 module.exports = calculateTotalSpentByCategory;
+
